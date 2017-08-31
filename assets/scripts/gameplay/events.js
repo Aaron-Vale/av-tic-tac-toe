370b8e85e25baf357474decc47e9bfae4fb1e6df
A-Aron <vale.aaron@gmail.com>
2017-08-30 22:50:05 -0400

    Add Game Engine Functions
    Added functionality to alternate turns between X and O, as well as logic
    to check for a winner. When a winner is detected, the winning squares
    change color and the game prevents any further moves.

--- assets/scripts/gameplay/events.js
+++ assets/scripts/gameplay/events.js
@@ -1,10 +1,22 @@
 'use strict'
+const logic = require('./logic')
 
 const onClickSquare = function () {
   if (!$(this).hasClass('played')) {
-    $(this).html('<p class="move">X</p>')
+    const squareId = this.id
+    const letterToPlay = logic.whoseTurn
+    $(this).html('<p class="move">' + letterToPlay + '</p>')
+    logic.boardData[squareId] = letterToPlay
+    logic.whoseTurn = (letterToPlay === 'x' ? 'o' : 'x')
   }
   $(this).addClass('played')
+  const winner = logic.checkForWinner()
+  if (winner.length > 0) {
+    $('.board-square').off() // No more moves allowed
+    $('#' + winner[0]).css('background-color', 'gold')
+    $('#' + winner[1]).css('background-color', 'gold')
+    $('#' + winner[2]).css('background-color', 'gold')
+  }
 }
 const setEventListeners = function () {
   $('.board-square').on('click', onClickSquare)
--- assets/scripts/gameplay/logic.js
+++ assets/scripts/gameplay/logic.js
@@ -1,5 +1,37 @@
 'use strict'
 
-const switchTurn = function () {
-  i
+const boardData = ['', '', '', '', '', '', '', '', '']
+let whoseTurn = 'x'
+
+const checkForWinner = function () {
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  const winningNumbers = []
+  for (let i = 0; i < winningCombos.length; i++) {
+    const val1 = winningCombos[i][0]
+    const val2 = winningCombos[i][1]
+    const val3 = winningCombos[i][2]
+    if (boardData[val1] !== '' && boardData[val2] !== '' && boardData[val3] !== '') {
+      if (boardData[val1] === boardData[val2] && boardData[val2] === boardData[val3]) {
+        winningNumbers.push(val1)
+        winningNumbers.push(val2)
+        winningNumbers.push(val3)
+      }
+    }
+  }
+  return winningNumbers
+}
+
+module.exports = {
+  boardData,
+  whoseTurn,
+  checkForWinner
 }
